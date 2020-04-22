c434e214d072784eff76f01f5804cbe9d41d468f
mikaelpasovski <mikaelpasovski@gmail.com>
2020-04-22 10:54:30 +0200

    Added database driver and stuff

---
+++ Test/com/nullxdeadbeef/Service/DatabaseAdapterTest.java
@@ -0,0 +1,7 @@
+package com.nullxdeadbeef.Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseAdapterTest {
+
+}
--- src/com/nullxdeadbeef/FlyRejse.java
+++ src/com/nullxdeadbeef/FlyRejse.java
@@ -1,4 +1,62 @@
 package com.nullxdeadbeef;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class FlyRejse {
+
+    private Date dato;
+    private Boolean arrival;
+    private String ruteNr;
+    private Time scheduledTime;
+    private String origin_destination;
+    private Fly fly;
+
+    public Date getDato() {
+        return dato;
+    }
+
+    public void setDato(Date dato) {
+        this.dato = dato;
+    }
+
+    public Boolean getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(Boolean arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getRuteNr() {
+        return ruteNr;
+    }
+
+    public void setRuteNr(String ruteNr) {
+        this.ruteNr = ruteNr;
+    }
+
+    public Time getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public void setScheduledTime(Time scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+
+    public String getOrigin_destination() {
+        return origin_destination;
+    }
+
+    public void setOrigin_destination(String origin_destination) {
+        this.origin_destination = origin_destination;
+    }
+
+    public Fly getFly() {
+        return fly;
+    }
+
+    public void setFly(Fly fly) {
+        this.fly = fly;
+    }
 }
