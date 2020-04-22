53e358956f18c67f0729f9baf6860a53091f91e9
Niklas <nikl4486@stud.kea.dk>
2020-04-22 13:20:12 +0200

    tårn

--- src/com/nullxdeadbeef/KontrolTårn.java
+++ src/com/nullxdeadbeef/KontrolTårn.java
@@ -1,27 +1,68 @@
 package com.nullxdeadbeef;
 
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class KontrolTårn {
     private int port;
-    private List<Fly> flyListe;
+    private ArrayList<Fly> flyListe;
+    private ArrayList<Fly> aktuelleFly;
+    private ArrayList<Fly> forbundedeFly;
+    private LocalDateTime tidspunkt;
+    private Socket server;
 
     public void printLog() {}
     public void sendTilAlle() {}
+    public void modtagBesked() {}
+    public void sendBesked() {}
 
     public int getPort() {
         return port;
     }
 
-    public void setPort( int port ) {
+    public void setPort(int port) {
         this.port = port;
     }
 
-    public List<Fly> getFlyListe() {
+    public ArrayList<Fly> getFlyListe() {
         return flyListe;
     }
 
-    public void setFlyListe( List<Fly> flyListe ) {
+    public void setFlyListe(ArrayList<Fly> flyListe) {
         this.flyListe = flyListe;
+    }
+
+    public ArrayList<Fly> getAktuelleFly() {
+        return aktuelleFly;
+    }
+
+    public void setAktuelleFly(ArrayList<Fly> aktuelleFly) {
+        this.aktuelleFly = aktuelleFly;
+    }
+
+    public ArrayList<Fly> getForbundedeFly() {
+        return forbundedeFly;
+    }
+
+    public void setForbundedeFly(ArrayList<Fly> forbundedeFly) {
+        this.forbundedeFly = forbundedeFly;
+    }
+
+    public LocalDateTime getTidspunkt() {
+        return tidspunkt;
+    }
+
+    public void setTidspunkt(LocalDateTime tidspunkt) {
+        this.tidspunkt = tidspunkt;
+    }
+
+    public Socket getServer() {
+        return server;
+    }
+
+    public void setServer(Socket server) {
+        this.server = server;
     }
 }
