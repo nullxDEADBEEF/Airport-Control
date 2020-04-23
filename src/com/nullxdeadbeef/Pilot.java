b50deac846e25a6c057635b2fe5dcdf8dd01d55e
nullxDEADBEEF <andxdgu@gmail.com>
2020-04-23 13:36:28 +0200

    Kommunikation mellem pilot og kontroltaarn WIP

--- src/com/nullxdeadbeef/Pilot.java
+++ src/com/nullxdeadbeef/Pilot.java
@@ -1,5 +1,8 @@
 package com.nullxdeadbeef;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class Pilot extends Thread {
@@ -9,10 +12,35 @@ public class Pilot extends Thread {
     @Override
     public void run() {
         sendBesked( "FORBIND_FLY " + fly.getaC() + ", " + fly.getFlyRejse().getRuteNr() );
+        modtagBesked();
+
     }
 
-    public void sendBesked( String besked ) {}
-    public void modtagBesked( String besked ) {}
+    public void sendBesked( String besked ) {
+        try {
+            DataOutputStream outputStream =
+                    new DataOutputStream( socket.getOutputStream() );
+            outputStream.writeUTF( besked );
+        } catch ( IOException ex ) {
+            ex.printStackTrace();
+            System.exit( 1 );
+        }
+
+    }
+    public void modtagBesked() {
+        try {
+            DataInputStream inputStream =
+                    new DataInputStream( socket.getInputStream() );
+            String besked = inputStream.readUTF();
+            if ( besked.contains( "FORBIND" ) ) {
+                // TODO: check if right airplane
+
+            }
+        } catch ( IOException ex ) {
+            ex.printStackTrace();
+            System.exit( 1 );
+        }
+    }
 
     public void mayday() {}
 
